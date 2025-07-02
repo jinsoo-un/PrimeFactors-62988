e899000326609f140150cef119629064f48871b7
see.jin <bobstar0@naver.com>
2025-07-02 10:51:52 +0900

    of12

--- PrimeFactorKATA/prime-factors.cpp
+++ PrimeFactorKATA/prime-factors.cpp
@@ -7,36 +7,14 @@ public:
 	vector<int> of(int number) {
 		vector<int> result = {};
 
-		if (number > 1) {
-			int divisor = 2;
-
-			if (number == 4 ||number == 6 || number == 9) {
-				for(divisor = 2; number> 1; divisor++) {
-					while (number % divisor == 0) {
-						result.push_back(divisor);
-						number /= divisor;
-					}
-				}
-			}
-			else {
-				result.push_back(number);
+		for (int divisor = 2; number > 1; divisor++) {
+			while (number % divisor == 0) {
+				result.push_back(divisor);
+				number /= divisor;
 			}
 		}
 
 		return result;
-
-		//if (number < 2) {
-		//	return 0; // No prime factors for numbers less than 2
-		//}
-		//int factor = 2;
-		//while (number > 1) {
-		//	if (number % factor == 0) {
-		//		number /= factor;
-		//	} else {
-		//		factor++;
-		//	}
-		//}
-		//return factor; // Return the last found prime factor
 	}
 private:
 

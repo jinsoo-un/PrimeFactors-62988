d632a15ce11ca6711489d13c9e9ebdc045493a34
see.jin <bobstar0@naver.com>
2025-07-02 10:46:33 +0900

    of1

--- PrimeFactorKATA/prime-factors.cpp
+++ PrimeFactorKATA/prime-factors.cpp
@@ -6,15 +6,20 @@ class PrimeFactors {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-
-		for (int divisor = 2; number > 1; divisor++) {
-			while (number % divisor == 0) {
-				result.push_back(divisor);
-				number /= divisor;
-			}
-		}
-
 		return result;
+
+		//if (number < 2) {
+		//	return 0; // No prime factors for numbers less than 2
+		//}
+		//int factor = 2;
+		//while (number > 1) {
+		//	if (number % factor == 0) {
+		//		number /= factor;
+		//	} else {
+		//		factor++;
+		//	}
+		//}
+		//return factor; // Return the last found prime factor
 	}
 private:
 
